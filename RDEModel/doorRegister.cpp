746fdb9720e59ee119b31a8bfcb9e5d204456e2f
francisc072 <168702670+francisc072@users.noreply.github.com>
2024-05-09 19:39:10 -0500

    Update doorRegister.cpp

--- RDEModel/doorRegister.cpp
+++ RDEModel/doorRegister.cpp
@@ -5,6 +5,45 @@
 #include "pch.h"
 #include "doorRegister.h"
 
-/**
- * doorRegister implementation
- */
+using namespace RDEModel;
+
+doorRegister::doorRegister() {}
+
+doorRegister::doorRegister(int entryTime, int exitTime, bool userIn, int code) {
+    this->entryTime = entryTime;
+    this->exitTime = exitTime;
+    this->userIn = userIn;
+    this->code = code;
+}
+
+int doorRegister::getEntryTime() {
+    return this->entryTime;
+}
+
+void doorRegister::setEntryTime(int entryTime) {
+    this->entryTime = entryTime;
+}
+
+int doorRegister::getExitTime() {
+    return this->exitTime;
+}
+
+void doorRegister::setExitTime(int exitTime) {
+    this->exitTime = exitTime;
+}
+
+bool doorRegister::isUserIn() {
+    return this->userIn;
+}
+
+void doorRegister::setUserIn(bool userIn) {
+    this->userIn = userIn;
+}
+
+int doorRegister::getCode() {
+    return this->code;
+}
+
+void doorRegister::setCode(int code) {
+    this->code = code;
+}
