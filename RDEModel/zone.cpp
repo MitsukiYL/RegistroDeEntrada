867a8cd33d4f969e762b127965e9e0cc44d884ae
francisc072 <168702670+francisc072@users.noreply.github.com>
2024-05-09 20:07:01 -0500

    Update zone.cpp

--- RDEModel/zone.cpp
+++ RDEModel/zone.cpp
@@ -5,6 +5,55 @@
 #include "pch.h"
 #include "zone.h"
 
-/**
- * zone implementation
- */
+using namespace RDEModel;
+
+zone::zone() {}
+
+zone::zone(String^ location, String^ name, int openingTime, int closingTime,
+           bool active) {
+    this->location = location;
+    this->name = name;
+    this->openingTime = openingTime;
+    this->closingTime = closingTime;
+    this->active = active;
+}
+
+String^ zone::getLocation() {
+    return this->location;
+}
+
+void zone::setLocation(String^ location) {
+    this->location = location;
+}
+
+String^ zone::getName() {
+    return this->name;
+}
+
+void zone::setName(String^ name) {
+    this->name = name;
+}
+
+int zone::getOpeningTime() {
+    return this->openingTime;
+}
+
+void zone::setOpeningTime(int openingTime) {
+    this->openingTime = openingTime;
+}
+
+int zone::getClosingTime() {
+    return this->closingTime;
+}
+
+void zone::setClosingTime(int closingTime) {
+    this->closingTime = closingTime;
+}
+
+bool zone::getActive() {
+    return this->active;
+}
+
+void zone::setActive(bool active) {
+    this->active = active;
+}
