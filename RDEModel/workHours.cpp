c1e24d46de3874f6d7322030c605e87ada133b75
francisc072 <168702670+francisc072@users.noreply.github.com>
2024-05-09 20:05:47 -0500

    Update workHours.cpp

--- RDEModel/workHours.cpp
+++ RDEModel/workHours.cpp
@@ -5,6 +5,55 @@
 #include "pch.h"
 #include "workHours.h"
 
-/**
- * workHours implementation
- */
+using namespace RDEModel;
+
+workHours::workHours() {}
+
+workHours::workHours(int inTime, int outTime, int breakStartTime, int breakFinishTime,
+                     String^ scheduleDay) {
+    this->inTime = inTime;
+    this->outTime = outTime;
+    this->breakStartTime = breakStartTime;
+    this->breakFinishTime = breakFinishTime;
+    this->scheduleDay = scheduleDay;
+}
+
+int workHours::getInTime() {
+    return this->inTime;
+}
+
+void workHours::setInTime(int inTime) {
+    this->inTime = inTime;
+}
+
+int workHours::getOutTime() {
+    return this->outTime;
+}
+
+void workHours::setOutTime(int outTime) {
+    this->outTime = outTime;
+}
+
+int workHours::getBreakStartTime() {
+    return this->breakStartTime;
+}
+
+void workHours::setBreakStartTime(int breakStartTime) {
+    this->breakStartTime = breakStartTime;
+}
+
+int workHours::getBreakFinishTime() {
+    return this->breakFinishTime;
+}
+
+void workHours::setBreakFinishTime(int breakFinishTime) {
+    this->breakFinishTime = breakFinishTime;
+}
+
+String^ workHours::getScheduleDay() {
+    return this->scheduleDay;
+}
+
+void workHours::setScheduleDay(String^ scheduleDay) {
+    this->scheduleDay = scheduleDay;
+}
