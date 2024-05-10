2ce6ae0c00007184a4601dcce2225459489da41a
francisc072 <168702670+francisc072@users.noreply.github.com>
2024-05-09 19:49:40 -0500

    Update parkingLot.cpp

--- RDEModel/parkingLot.cpp
+++ RDEModel/parkingLot.cpp
@@ -5,6 +5,63 @@
 #include "pch.h"
 #include "parkingLot.h"
 
-/**
- * parkingLot implementation
- */
+using namespace RDEModel;
+
+parkingLot::parkingLot() {}
+
+parkingLot::parkingLot(String^ name, char vehicleType, String^ ID, int capacity, int N_reserved, int N_inactive) {
+    this->name = name;
+    this->vehicleType = vehicleType;
+    this->ID = ID;
+    this->capacity = capacity;
+    this->N_reserved = N_reserved;
+    this->N_inactive = N_inactive;
+}
+
+String^ parkingLot::getname() {
+    return this->name;
+}
+
+void parkingLot::setname(String^ name) {
+    this->name = name;
+}
+
+char parkingLot::getvehicleType() {
+    return this->vehicleType;
+}
+
+void parkingLot::setvehicleType(char vehicleType) {
+    this->vehicleType = vehicleType;
+}
+
+String^ parkingLot::getID() {
+    return this->ID;
+}
+
+void parkingLot::setID(String^ ID) {
+    this->ID = ID;
+}
+
+int parkingLot::getcapacity() {
+    return this->capacity;
+}
+
+void parkingLot::setcapacity(int capacity) {
+    this->capacity = capacity;
+}
+
+int parkingLot::getNreserved() {
+    return this->N_reserved;
+}
+
+void parkingLot::setNreserved(int N_reserved) {
+    this->N_reserved = N_reserved;
+}
+
+int parkingLot::getNinactive() {
+    return this->N_inactive;
+}
+
+void parkingLot::setNinactive(int N_inactive) {
+    this->N_inactive = N_inactive;
+}
