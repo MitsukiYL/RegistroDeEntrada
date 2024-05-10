0cb60136ca6d9d87cfa9242957e802680b9d62d2
francisc072 <168702670+francisc072@users.noreply.github.com>
2024-05-09 19:51:13 -0500

    Update parkingSite.cpp

--- RDEModel/parkingSite.cpp
+++ RDEModel/parkingSite.cpp
@@ -5,6 +5,36 @@
 #include "pch.h"
 #include "parkingSite.h"
 
-/**
- * parkingSite implementation
- */
+using namespace RDEModel;
+
+parkingSite::parkingSite() {}
+
+parkingSite::parkingSite(String^ ID, bool reserved, bool active) {
+    this->ID = ID;
+    this->reserved = reserved;
+    this->active = active;
+}
+
+String^ parkingSite::getID() {
+    return this->ID;
+}
+
+void parkingSite::setID(String^ ID) {
+    this->ID = ID;
+}
+
+bool parkingSite::isReserved() {
+    return this->reserved;
+}
+
+void parkingSite::setReserved(bool reserved) {
+    this->reserved = reserved;
+}
+
+bool parkingSite::isActive() {
+    return this->active;
+}
+
+void parkingSite::setActive(bool active) {
+    this->active = active;
+}
