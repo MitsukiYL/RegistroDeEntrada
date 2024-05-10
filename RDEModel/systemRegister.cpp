ac0143275ee712cff61334fc5e900400d2321e07
francisc072 <168702670+francisc072@users.noreply.github.com>
2024-05-09 20:01:47 -0500

    Update systemRegister.cpp

--- RDEModel/systemRegister.cpp
+++ RDEModel/systemRegister.cpp
@@ -5,6 +5,36 @@
 #include "pch.h"
 #include "systemRegister.h"
 
-/**
- * systemRegister implementation
- */
+using namespace RDEModel;
+
+systemRegister::systemRegister() {}
+
+systemRegister::systemRegister(int date, String^ operationType, String^ IP) {
+    this->date = date;
+    this->operationType = operationType;
+    this->IP = IP;
+}
+
+int systemRegister::getDate() {
+    return this->date;
+}
+
+void systemRegister::setDate(int date) {
+    this->date = date;
+}
+
+String^ systemRegister::getOperationType() {
+    return this->operationType;
+}
+
+void systemRegister::setOperationType(String^ operationType) {
+    this->operationType = operationType;
+}
+
+String^ systemRegister::getIP() {
+    return this->IP;
+}
+
+void systemRegister::setIP(String^ IP) {
+    this->IP = IP;
+}
