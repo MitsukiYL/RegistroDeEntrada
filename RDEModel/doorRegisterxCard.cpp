0eece2dfff498cf481f3b730aa0149e7f7eae7fe
Diego Mora Jaimes <a20212434@pucp.edu.pe>
2024-06-01 18:29:18 -0500

    clase doorRegisterxCard

--- RDEModel/doorRegisterxCard.cpp
+++ RDEModel/doorRegisterxCard.cpp
@@ -1,3 +1,32 @@
-#pragma once
 
-//prueba 2
+
+#include "pch.h"
+#include "doorRegisterxCard.h"
+
+using namespace RDEModel;
+
+doorRegisterxCard::doorRegisterxCard() {}
+
+doorRegisterxCard::doorRegisterxCard(int codeCard, int codeDoorRegister) {
+
+	this->codeCard = codeCard;
+	this->codeDoorRegister = codeDoorRegister;
+}
+
+int doorRegisterxCard::getCodeCard() {
+	return this->codeCard;
+}
+
+void doorRegisterxCard::setCodeCard(int codeCard) {
+	this->codeCard = codeCard;
+}
+
+int doorRegisterxCard::getCodeDoorRegister() {
+	return this->codeDoorRegister;
+}
+
+void doorRegisterxCard::setCodeDoorRegister(int codeDoorRegister) {
+	this->codeDoorRegister = codeDoorRegister;
+}
+
+
--- RDEModel/doorRegisterxCard.h
+++ RDEModel/doorRegisterxCard.h
@@ -1,3 +1,23 @@
+
 #pragma once
 
-//prueba 2
+using namespace System;
+
+namespace RDEModel {
+
+	public ref class doorRegisterxCard {
+	private:
+		int codeCard;//codigo unico de la tarjeta
+		int codeDoorRegister;//una entrada y salida que una persona tuvo
+
+	public:
+		doorRegisterxCard();
+		doorRegisterxCard(int codeCard, int codeDoorRegister);
+
+		int getCodeCard();
+		void setCodeCard(int codeCard);
+
+		int getCodeDoorRegister();
+		void setCodeDoorRegister(int codeDoorRegister);
+	};
+}
