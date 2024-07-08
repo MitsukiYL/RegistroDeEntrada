80fbaf9960ab2485c7c8cd09cf97abd78ce44964
Mitsuki Yasuda <myasuda2011@hotmail.com>
2024-07-08 18:07:53 -0500

    avance

--- RDEView/InterfaceProfile.h
+++ RDEView/InterfaceProfile.h
@@ -66,7 +66,7 @@ namespace RDEView {
 
 
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
 
 
 	private: System::Windows::Forms::Label^ label8;
@@ -90,8 +90,14 @@ namespace RDEView {
 
 	private: System::Windows::Forms::Label^ label9;
 	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridView^ Vehicle_DGV;
+
 	private: System::Windows::Forms::Label^ label10;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column3;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column2;
+
+
 
 	private:
 		/// <summary>
@@ -113,7 +119,6 @@ namespace RDEView {
 			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->label8 = (gcnew System::Windows::Forms::Label());
 			this->label7 = (gcnew System::Windows::Forms::Label());
 			this->txt_name = (gcnew System::Windows::Forms::TextBox());
@@ -126,10 +131,12 @@ namespace RDEView {
 			this->txt_password = (gcnew System::Windows::Forms::TextBox());
 			this->label9 = (gcnew System::Windows::Forms::Label());
 			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->Vehicle_DGV = (gcnew System::Windows::Forms::DataGridView());
+			this->Column1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->label10 = (gcnew System::Windows::Forms::Label());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Vehicle_DGV))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -207,16 +214,6 @@ namespace RDEView {
 			this->button1->Text = L"Actualizar datos";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &InterfaceProfile::button1_Click);
-			// 
-			// pictureBox1
-			// 
-			this->pictureBox1->BackColor = System::Drawing::Color::CadetBlue;
-			this->pictureBox1->Location = System::Drawing::Point(557, 52);
-			this->pictureBox1->Margin = System::Windows::Forms::Padding(4);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(159, 128);
-			this->pictureBox1->TabIndex = 12;
-			this->pictureBox1->TabStop = false;
 			// 
 			// label8
 			// 
@@ -328,21 +325,46 @@ namespace RDEView {
 			this->button2->Visible = false;
 			this->button2->Click += gcnew System::EventHandler(this, &InterfaceProfile::button2_Click);
 			// 
-			// dataGridView1
+			// Vehicle_DGV
 			// 
-			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(440, 222);
-			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->RowHeadersWidth = 51;
-			this->dataGridView1->RowTemplate->Height = 24;
-			this->dataGridView1->Size = System::Drawing::Size(386, 147);
-			this->dataGridView1->TabIndex = 25;
+			this->Vehicle_DGV->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->Vehicle_DGV->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(3) {
+				this->Column1,
+					this->Column3, this->Column2
+			});
+			this->Vehicle_DGV->Location = System::Drawing::Point(440, 82);
+			this->Vehicle_DGV->Name = L"Vehicle_DGV";
+			this->Vehicle_DGV->RowHeadersWidth = 51;
+			this->Vehicle_DGV->RowTemplate->Height = 24;
+			this->Vehicle_DGV->Size = System::Drawing::Size(386, 147);
+			this->Vehicle_DGV->TabIndex = 25;
+			// 
+			// Column1
+			// 
+			this->Column1->HeaderText = L"Placa";
+			this->Column1->MinimumWidth = 6;
+			this->Column1->Name = L"Column1";
+			this->Column1->Width = 125;
+			// 
+			// Column3
+			// 
+			this->Column3->HeaderText = L"Marca";
+			this->Column3->MinimumWidth = 6;
+			this->Column3->Name = L"Column3";
+			this->Column3->Width = 125;
+			// 
+			// Column2
+			// 
+			this->Column2->HeaderText = L"Modelo";
+			this->Column2->MinimumWidth = 6;
+			this->Column2->Name = L"Column2";
+			this->Column2->Width = 125;
 			// 
 			// label10
 			// 
 			this->label10->AutoSize = true;
 			this->label10->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
-			this->label10->Location = System::Drawing::Point(436, 192);
+			this->label10->Location = System::Drawing::Point(436, 54);
 			this->label10->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label10->Name = L"label10";
 			this->label10->Size = System::Drawing::Size(206, 20);
@@ -355,7 +377,7 @@ namespace RDEView {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(848, 415);
 			this->Controls->Add(this->label10);
-			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->Vehicle_DGV);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->txt_password);
 			this->Controls->Add(this->label9);
@@ -368,7 +390,6 @@ namespace RDEView {
 			this->Controls->Add(this->txt_name);
 			this->Controls->Add(this->label7);
 			this->Controls->Add(this->label8);
-			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label6);
 			this->Controls->Add(this->label5);
@@ -380,32 +401,49 @@ namespace RDEView {
 			this->Name = L"InterfaceProfile";
 			this->Text = L"Perfil de usuario";
 			this->Load += gcnew System::EventHandler(this, &InterfaceProfile::InterfaceProfile_Load);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Vehicle_DGV))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void InterfaceProfile_Load(System::Object^ sender, System::EventArgs^ e) {
-		if (this->objUser != nullptr) {
 
-			this->txt_name->Text = this->objUser->getPerson()->getName();
-			this->txt_code->Text = Convert::ToString(this->objUser->getPerson()->getCode());
-			this->txt_DNI->Text = Convert::ToString(this->objUser->getPerson()->getDNI());
-			this->txt_occ->Text = this->objUser->getPerson()->getOccupation();
-			this->txt_mail->Text = this->objUser->getPerson()->getMail();
-			this->txt_phone->Text = this->objUser->getPerson()->getPhone();
-
-			if (this->objUser->getParkingSite()->getID() == "PLACEHOLDER") {//SI EL USUARIO NO TIENE NINGUN PARKING SITE RELACIONADO
-				this->txt_parksite->Text = "Sin estacionamiento reservado";
-			}
-			else {
-				this->txt_parksite->Text = this->objUser->getParkingSite()->getID();
-			}
-			
+private: void mostrarGrillaVehicle(List<vehicle^>^ listVehicle) {
+	this->Vehicle_DGV->Rows->Clear();
+	for (int i = 0; i < listVehicle->Count; i++) {
+		vehicle^ objVehicle = listVehicle[i];
+		array<String^>^ filaGrilla = gcnew array<String^>(3);
+		if (objVehicle != nullptr) {
+			filaGrilla[0] = objVehicle->getPlate();
+			filaGrilla[1] = objVehicle->getBrand();
+			filaGrilla[2] = objVehicle->getModel();
 		}
+		this->Vehicle_DGV->Rows->Add(filaGrilla);
 	}
+}
+private: System::Void InterfaceProfile_Load(System::Object^ sender, System::EventArgs^ e) {
+	if (this->objUser != nullptr) {
+
+		VehicleCtrl^ objVehicleCtrl = gcnew VehicleCtrl();
+
+		this->txt_name->Text = this->objUser->getPerson()->getName();
+		this->txt_code->Text = Convert::ToString(this->objUser->getPerson()->getCode());
+		this->txt_DNI->Text = Convert::ToString(this->objUser->getPerson()->getDNI());
+		this->txt_occ->Text = this->objUser->getPerson()->getOccupation();
+		this->txt_mail->Text = this->objUser->getPerson()->getMail();
+		this->txt_phone->Text = this->objUser->getPerson()->getPhone();
+
+		if (this->objUser->getParkingSite()->getID() == "PLACEHOLDER") {//SI EL USUARIO NO TIENE NINGUN PARKING SITE RELACIONADO
+			this->txt_parksite->Text = "Sin estacionamiento reservado";
+		}
+		else {
+			this->txt_parksite->Text = this->objUser->getParkingSite()->getID();
+		}
+			
+		List<vehicle^>^ listVehicles = objVehicleCtrl->buscarVehiclexUser(this->objUser->getUserID());
+		mostrarGrillaVehicle(listVehicles);
+	}
+}
 private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 	if (change_state) {
 		this->txt_mail->Enabled = true;
